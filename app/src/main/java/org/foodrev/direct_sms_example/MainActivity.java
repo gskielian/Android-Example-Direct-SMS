f9162a4dc59ffc661c81271ec1200f9c1a474210
Gregory S. Kielian <gregory.kielian@gmail.com>
2016-08-23 09:57:58 -0700

    add permissions check and background sms-send code

--- app/src/main/java/org/foodrev/direct_sms_example/MainActivity.java
+++ app/src/main/java/org/foodrev/direct_sms_example/MainActivity.java
@@ -1,15 +1,26 @@
 package org.foodrev.direct_sms_example;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.telephony.SmsManager;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private String phoneNumber;
+    private String message;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,14 +29,73 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        fab.setOnClickListener(
+
+
+        new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                phoneNumber = "4154652008";
+                message = "hi from phone";
+
+                Snackbar.make(view, "sending message: " + message + " to phone number: " + phoneNumber, Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
+
+                sendSMSAfterPermissionsCheck(phoneNumber,message);
             }
-        });
+        }
+        );
+    }
+
+
+
+
+
+
+    private void sendSMSAfterPermissionsCheck(String phoneNumber, String message) {
+        int hasSendSMSPermission = ContextCompat.checkSelfPermission(MainActivity.this,
+                Manifest.permission.SEND_SMS);
+        if (hasSendSMSPermission!= PackageManager.PERMISSION_GRANTED) {
+            String[] permissions = new String[]{Manifest.permission.SEND_SMS};
+            ActivityCompat.requestPermissions(MainActivity.this, permissions, 1);
+        }
+        else {
+            SmsManager sms = SmsManager.getDefault();
+            sms.sendTextMessage(phoneNumber, null, message, null, null);
+        }
+    }
+
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case 1: {
+
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                    SmsManager sms = SmsManager.getDefault();
+                    sms.sendTextMessage(phoneNumber, null, message, null, null);
+
+                    // permission was granted, yay! Do the
+                    // contacts-related task you need to do.
+                } else {
+
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                    Toast.makeText(MainActivity.this, "Permission denied to read your External storage", Toast.LENGTH_SHORT).show();
+                }
+                return;
+            }
+
+            // other 'case' lines to check for other
+            // permissions this app might request
+        }
     }
 
     @Override
